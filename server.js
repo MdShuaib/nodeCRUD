e80ab72f868fbb968e1a50a751d9a15c1e41dcc1
Vedprakash arya <vedprakash@kwd.co.in>
2017-09-14 15:09:16 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(express.static('./public'));
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 
 app.all('/*', function(req, res, next) {
-       	res.header('Access-Control-Allow-Origin': '*');
-       	res.header('Access-Control-Allow-Methods': "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-		res.header('Access-Control-Allow-Headers': 'Content-Type, Accept, X-Access-Token');
-		
+       	res.header("Access-Control-Allow-Origin", "*");
+       	res.header('Access-Control-Allow-Methods', "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  		res.header("Access-Control-Allow-Headers",'Content-Type, Accept, X-Access-Token');
+       
 		if(req.method == 'OPTIONS') {
        		 console.log("Options is : " + req.method);
 			res.status(200).end();
